c12a4dc8dad215d75fb29b79a4c8615c84449ce1
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-05 15:38:42 -0700

    Setup Navbar WireFrame

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function navbar() {
     return (
         <div className="navbar">
-
+            <div className="logo">
+                <h2>Beaches</h2>
+            </div>
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>Travel</li>
+                <li>Book</li>
+                <li>Views</li>
+            </ul>
         </div>
     )
 }
